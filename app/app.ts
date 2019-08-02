37bc9f34a04275c3cf46d9d9c2e93d5c872df292
csteinmetz1 <csteinmetz1@gmail.com>
2019-08-02 15:26:47 -0400

    adding serve of static webpage

--- app/app.ts
+++ app/app.ts
@@ -18,14 +18,15 @@
 */ 
 import express from 'express'; // Express web server framework
 import * as request from 'request'; // used for http requests
+import path from 'path';
 //var querystring = require('querystring'); 
 //var cookieParser = require('cookie-parser');
 //var session = require('client-sessions'); // store user data in cookies
 //var fs = require('fs'); // used to read and write to the local filesystem 
-//var socket = require('socket.io'); // sockect connection to clients
+//var socket = require('socket.io'); // socket connection to clients
 //var bodyparser = require('body-parser'); // 
 //var favicon = require('serve-favicon');
-//var path = require('path'); //local path for filesystem read and writes
+
 
 import { getToken } from './Classes/SpotifyAuthentication';
 
@@ -37,6 +38,7 @@ const PORT = 8000;
 
 // Create a new express application instance
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
   res.send('This is adv');
