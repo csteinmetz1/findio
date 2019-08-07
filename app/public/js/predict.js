bd74df087304413960955e1e6a0ba76303491677
csteinmetz1 <csteinmetz1@gmail.com>
2019-08-07 13:35:18 -0400

    adding new UI features to the main page

--- app/public/js/predict.js
+++ app/public/js/predict.js
@@ -1,7 +1,10 @@
 
 var predict = function(userImage, model) {
-	const userImageTensor = tf.browser.fromPixels(userImage);
-	const prediction = model.predict(userImageTensor.expandDims(0));
-	console.log(prediction.array())
+	return new Promise( function (resolve, reject) {
+		const userImageTensor = tf.browser.fromPixels(userImage);
+		const prediction = model.predict(userImageTensor.expandDims(0));
+		console.log(prediction.array());
+		resolve(prediction.array());
+	});
 }
 
