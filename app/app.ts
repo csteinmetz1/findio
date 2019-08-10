4216cff72fe8df1b1ab2a8135591c27269f9fbb9
csteinmetz1 <csteinmetz1@gmail.com>
2019-08-10 16:13:39 -0400

    expanding filtering/search logic to filter based on given audio feature

--- app/app.ts
+++ app/app.ts
@@ -22,15 +22,15 @@ import path from 'path';
 /*
     Local Imports
 */
-import { getToken } from './Classes/SpotifyAuthentication';
-import { search } from './Classes/SpotifySearch';
+import { getToken, checkToken } from './Classes/SpotifyAuthentication';
+import { search, getAudioFeatures } from './Classes/SpotifyConnector';
 
 /* 
     Constants
 */
 //port used for the server
 const PORT = 8000;
-let token: string = "";
+const audioFeatureVector = ['danceability', 'energy', 'mode', 'acousticness', 'instrumentalness', 'liveness', 'valence'];
 
 // Create a new express application instance
 const app = express();
@@ -39,25 +39,51 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
-// application requests authorization
-getToken().then(
-  value =>  {
-    console.log(value);
-    token = value;
-  }, reason => {
-    console.log(reason);
-  }
-);
+var subtractVectors = function(a:number[], b:number[]) {
+  return a.map(function(element:number, index:number){
+    return element - b[index];
+  })
+}
+
 
 app.post('/search', (req, res) => {
-  search(req.body.query, req.body.startYear, req.body.stopYear, token).then(
-    result => {
-      res.render('results.ejs', { results : result.tracks.items, query : req.body.query} )
-    }, error => {
-      console.log(error);
-      res.render('error.ejs', {error: error})
+
+  // application requests authorization
+  getToken()
+  .then(function(token:string) {
+    console.log(token)
+    const searchAudioFeatures = [req.body.danceability, req.body.energy, req.body.mode, req.body.acousticness, req.body.instrumentalness, req.body.liveness, req.body.valence];
+    const searchAudioFeatureTolerance = req.body.tolerance;
+
+    let featureVectorWithinTolerance = function(track:any) {
+      let difference = subtractVectors(track.audio_features, searchAudioFeatures);
+      let distance = difference.reduce( (acc, cur) => acc + Math.pow(cur,2) );
+      console.log("distance: " + distance + "tolerance: " + searchAudioFeatureTolerance);
+      if (distance < searchAudioFeatureTolerance) {
+        return true;
+      }
     }
-  );
+    search(req.body.query, req.body.startYear, req.body.stopYear, token)
+    .then(function(result) {
+      let trackIds:string[] = result.tracks.items.map( (track:any) => track.id );
+      getAudioFeatures(trackIds, token)
+      .then(function(audioFeatures) {
+        audioFeatures.map( 
+          function(f:any, index:number) {
+            let featureVector = [f.danceability, f.energy, f.mode, f.acousticness, f.instrumentalness, f.liveness, f.valence] 
+            result.tracks.items[index].audio_features = featureVector;
+          });
+
+        let filteredTracks = result.tracks.items.filter(featureVectorWithinTolerance) 
+        res.render('results.ejs', { results : filteredTracks, query : req.body.query} )
+      });   
+      }, error => {
+        console.log(error);
+        res.render('error.ejs', {error: error})
+      });
+    }, function(reason) {
+      console.log(reason);
+  })
 })
 
 app.listen(PORT, function () {
