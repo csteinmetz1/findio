19b0cc52b8374c3a2539f4db6143c23eae7d8cfe
csteinmetz1 <csteinmetz1@gmail.com>
2019-08-21 21:01:19 -0400

    working on the now playing bar - temp fix to quotes bug and adding in transition so fade in and out on stoping/starting playback

--- app/public/js/previews.js
+++ app/public/js/previews.js
@@ -13,20 +13,36 @@ function playAudio(id, artist, track, cover) {
 	// play the desired track
 	let player = document.getElementById("audio" + id); 
 	let button = document.getElementById("button" + id);
+	let playing = false;	// track wether something is playing
 	if (player.paused) {
 	  player.play(); 
+	  playing = true;
 	  button.innerHTML = "Pause preview";
 	}
 	else {
 	  player.pause(); 
 	  button.innerHTML = "Play preview";
+	  playing = false;
 	}
 
 	// update now playing
+	let footer     = document.getElementById("footer");
 	let albumCover = document.getElementById("cover");
 	let artistName = document.getElementById("artist");
 	let trackName  = document.getElementById("track");
-	albumCover.src = cover;
-	artistName.innerHTML = artist;
-	trackName.innerHTML = track;
+	let trackUrl   = document.getElementById("trackUrl")
+
+	if (playing) {
+	  footer.classList.remove("m-fadeOut");
+	  footer.classList.add("m-fadeIn");
+	  albumCover.src = cover;
+	  artistName.innerHTML = artist;
+	  trackName.innerHTML = track;
+	  trackUrl.href = "https://open.spotify.com/track/" + id;
+	}
+	else {
+	  footer.classList.remove("m-fadeIn");
+	  footer.classList.add("m-fadeOut");
+	}
+	
 } 
