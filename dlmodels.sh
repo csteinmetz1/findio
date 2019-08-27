478243e12c9badd06746f8ea4e1bf7373c243bd7
csteinmetz1 <csteinmetz1@gmail.com>
2019-08-27 10:37:49 -0500

    updates to allow for model to be downloaded during build

---
+++ dlmodels.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1T_47s7UJBDGx9SPhXxSXf--58-FrQOzF' -O models.zip
+unzip ./models.zip -d ./build/public/models
+rm models.zip
